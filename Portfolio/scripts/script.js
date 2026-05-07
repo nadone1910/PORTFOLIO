21bb5d391aa38992cb03d0a542595e5bbed0f95c
nadone1910 <nadonesamuel55@gmail.com>
2026-05-07 00:12:39 +0000

    arrumei a data de formatura

--- Portfolio/scripts/script.js
+++ Portfolio/scripts/script.js
@@ -22,6 +22,8 @@ let curso = {
     disciplinaAtual: "Aluno"
 }
 
+
+
 console.log(typeof nulo);
 console.log(typeof indefinido);
 console.log(typeof anoFormatura);
@@ -34,9 +36,63 @@ document.getElementById("meuNome").innerText = NOME;
 document.getElementById("tituloProfissional").innerText = tituloProfissional;
 document.getElementById("minhaBio").innerText = minhaBio;
 document.getElementById("anoFormatura").innerText = anoFormatura;
-document.getElementById("anoIngresso").innerText = anoIngresso;
+document.getElementById("anoIngresso").innerText = "Ano de Ingresso: " + anoIngresso;
 document.getElementById("anoFormatura").innerText = "Ano de formatura: " + anoFormatura;
-document.getElementById("tempoRestanteParaFormatura").innerText = `Tempo restante para formatura ${anoFormatura - anoAtual} anos, ${mesFormatura - mesAtual} meses, ${diaFormatura - diaAtual} dias`;
+
+
+
+let diasRestantes = diaFormatura - diaAtual;
+let mesesRestantes = mesFormatura - mesAtual;
+let anosRestantes = anoFormatura - anoAtual;
+
+if (anoFormatura - anoAtual <= 0) {
+    document.getElementById("tempoRestanteParaFormatura").innerText = `Tempo para formatura: `;
+} else if (anoFormatura - anoAtual === 1) {
+    document.getElementById("tempoRestanteParaFormatura")
+    .innerText = `Tempo para formatura: ${anoFormatura - anoAtual} ano /`;
+} else {
+    document.getElementById("tempoRestanteParaFormatura")
+    .innerText = `Tempo para formatura: ${anoFormatura - anoAtual} anos /`;
+}
+
+
+if (mesFormatura - mesAtual <= 0) {
+    document.getElementById("tempoRestanteParaFormatura2").innerText = `- /`;
+} else if (mesFormatura - mesAtual === 1) {
+    document.getElementById("tempoRestanteParaFormatura2")
+    .innerText = ` ${mesFormatura - mesAtual} mes /`;
+} else {
+    document.getElementById("tempoRestanteParaFormatura2")
+    .innerText = ` ${mesFormatura - mesAtual} meses /`;
+}
+
+if (diaFormatura - diaAtual <= 0) {
+    document.getElementById("tempoRestanteParaFormatura3").innerText = `-`;
+} else if (diaFormatura - diaAtual === 1) {
+    document.getElementById("tempoRestanteParaFormatura3")
+    .innerText = `${diaFormatura - diaAtual} dia`;
+} else {
+    document.getElementById("tempoRestanteParaFormatura3")
+    .innerText = `${diaFormatura - diaAtual} dias`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Funcionalidade do botão "Sobre mim"
 const botao = document.querySelector('.botao');
